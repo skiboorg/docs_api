2a30e3134a8f751a15f3d04061c903df653e7fd4
skiboorg <d@skib.org>
2021-07-08 13:47:14 +0300

    add pack

--- user/views.py
+++ user/views.py
@@ -7,8 +7,10 @@ from django.http import HttpResponseRedirect
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-
-from .services import create_random_string
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from random import choices
+import string
 from .serializers import *
 from .models import *
 from rest_framework import generics
@@ -50,12 +52,22 @@ class GetUser(generics.RetrieveAPIView):
 class UserRecoverPassword(APIView):
     def post(self,request):
         user = None
+        print(request.data['email'])
         try:
-            user = User.objects.get(phone=request.data['phone'])
+            user = User.objects.get(email=request.data['email'])
         except:
             user = None
         if user:
-            messageSend = True
+            password=''.join(choices(string.digits, k=8))
+            print(password)
+            user.set_password(password)
+            user.save()
+            msg_html = render_to_string('new_password.html', {'pass': password})
+            send_mail('Ваш новый пароль docsuniform.ru', None, 'noreply@docsuniform.ru',
+                      [user.email,'dimon.skiborg@gmail.com'],
+                      fail_silently=False, html_message=msg_html)
             return Response({'result': True, 'email': user.email}, status=200)
+
+
         else:
             return Response({'result': False}, status=200)
